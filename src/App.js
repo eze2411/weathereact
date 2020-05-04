f73f11060de24bb465ae4489a0e3ee8ec195c798
eze2411 <eze2411@gmail.com>
2020-05-04 20:04:55 -0300

    instala redux, react-redux creacion store

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Grid, Col, Row } from 'react-flexbox-grid';
+import { createStore } from "redux";
 import LocationList from "./components/LocationList";
 import ForecastExtended from "./components/ForecastExtended";
 import './App.css';
@@ -16,6 +17,9 @@ const cities = [
     'Stockholm,SE'
 ];
 
+const store = createStore(() => {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 class App extends React.Component {
 
     constructor() {
@@ -25,6 +29,8 @@ class App extends React.Component {
     handleSelectedLocation = city => {
         this.setState({city});
         console.log(`handleSelectedLocation: ${city}`);
+        const action = {type: 'setCity', value: city};
+        store.dispatch(action);
     };
 
     render() {
